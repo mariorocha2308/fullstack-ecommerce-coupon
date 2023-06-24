2138872cd1ec18dd85b1102909d6366f2f3fbb2b
mariorocha2308 <virgoroch852@gmail.com>
2023-06-24 17:21:18 -0600

    change landing design

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Box, Image, Text } from '@chakra-ui/react'
-import image from '../assets/Halloween tickets-pana.svg'
+import image from '../assets/12.12 Sale-cuate.svg'
 
 const Landing = () => {
   return (  
-    <Box display='flex' py='8' justifyContent='space-evenly' alignItems='center'>
+    <Box display='flex' py='10' justifyContent='space-evenly' alignItems='center'>
       <Box display='column' >
-        <Text fontWeight='black' fontSize='35' mb='3'>Coupons!</Text>
-        <Text width='400px'>For you, the best hot sales rigth now. Enjoy, buy now!</Text>
+        <Text fontSize='30' mb='3' fontFamily='Monserrat'>Coupons Store!</Text>
+        <Text width='400px' fontFamily='Poppins-Bold' mb='0.5rem'>
+          Enjoy 24/7 special offers on our platform, 
+          we want you to be able to live reactively! 
+        </Text>
+        <Text width='400px' fontFamily='Poppins-Bold'>This and more benefits for our users.</Text>
       </Box>
       <Image src={image} height='300px' alt='halloween coupons'/>
     </Box>
